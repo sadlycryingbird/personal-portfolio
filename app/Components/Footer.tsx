941de923da7f978594a195fd6e03b3f83ea34314
Khizr Fazal <197388844+sadlycryingbird@users.noreply.github.com>
2025-02-08 03:27:01 +0000

    finished up footer barebones

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -2,6 +2,23 @@
 
 export default function Footer() {
     return (
-        <p>hi</p>
+        <footer>
+            <p>© {new Date().getFullYear()} Khizr Fazal. All rights reserved.</p>
+            <ul>
+                <li>
+                    <a href="https://github.com/sadlycryingbird" target="_blank" rel="noopener no referrer">
+                        Github
+                    </a>
+                </li>
+                <li>
+                    <a href="https://linkedin.com/in/khizr-fazal" target="_blank" rel="noopener noreferrer">
+                        LinkedIn                   
+                    </a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
+            </ul>
+        </footer>
     )
 }
