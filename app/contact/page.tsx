99650a0ded69817e87279656224f2b69951beee7
Khizr Fazal <197388844+sadlycryingbird@users.noreply.github.com>
2025-02-11 14:57:57 +0000

    added contact page with relevant links and styling

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Contact from '../components/Contact';
+import Layout from '../components/Layout';
+
+export default function AboutPage() {
+    return (
+        <Layout>
+            <Contact />;
+        </Layout>
+    )
+}
